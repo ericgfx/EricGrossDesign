f1e06b76470a20b6acdebde0f83ad2cd9af3c8a5
ericgfx <ericgfx01@gmail.com>
2018-10-08 15:57:10 -0700

    ScreenMaint Start

---
+++ Code For Sutter Screens/ScreenMaintenance.py
@@ -0,0 +1,14 @@
+slideList = [
+    {"filename": "Slide1.png",
+    "content": 'KeyDates',
+    "endDate": '03/03/2019',
+    "Filler Status": False,
+    "startDate":'01/01/2018'},
+    {"filename": "Slide2.png",
+    "content": 'MoveTeam',
+    "endDate": '03/03/2019',
+    "Filler Status": False,
+    "startDate":'01/01/2018'}
+]
+
+print slideList[1]["content"]
