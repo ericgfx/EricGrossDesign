0744022cfbe6ec8e4b7830dfa8fd6a1cee50cc58
ericgfx <ericgfx01@gmail.com>
2018-10-08 23:41:51 -0700

    Python for Screens

--- Code For Sutter Screens/ScreenMaintenance.py
+++ Code For Sutter Screens/ScreenMaintenance.py
@@ -1,14 +1,29 @@
-slideList = [
-    {"filename": "Slide1.png",
-    "content": 'KeyDates',
+import json
+
+filename = 'slideList.txt'
+
+def readFile():
+	file = open(filename, mode='r')
+	text = file.read()
+	file.close()
+	return text
+
+testList = [
+    {"filename": "Test1.png",
+    "content": 'TestContent1',
+    "startDate":'01/01/2018',
     "endDate": '03/03/2019',
-    "Filler Status": False,
-    "startDate":'01/01/2018'},
-    {"filename": "Slide2.png",
-    "content": 'MoveTeam',
-    "endDate": '03/03/2019',
-    "Filler Status": False,
-    "startDate":'01/01/2018'}
+    "Filler Status": False 
+    },
+    {
+    "filename": "Test2.png",
+    "content": 'TestContent2',
+	"startDate":'01/01/2018',
+    "endDate": '01/01/2018',
+    "Filler Status": False }
 ]
 
-print slideList[1]["content"]
+
+slideList = readFile()
+print(slideList[0]['content'])
+
