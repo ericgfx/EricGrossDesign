faa26a154590fa0a0a45bde8897f53cc2c7c2e0b
ericgfx <ericgfx01@gmail.com>
2018-10-15 16:50:55 -0700

    Success testing

--- CodeForSutterScreens/filenameChanger.py
+++ CodeForSutterScreens/filenameChanger.py
@@ -1,28 +1,37 @@
 import os
+from os.path import join, isfile
+
 # Global Variables for testing only.
 Directory = '.'
-OldFilename = 'Slide1.png'
+OldFilename = 'Slide01.png'
 NewFilename = 'Filler01.png'
 
 
-def join(Directory,Filename):
+def joinDirectoryAndName(Directory,Filename):
 	joint = os.path.join(Directory,Filename)
 	return joint
 
-def changeName(OldFilename,NewFilename,Directory):
-	OldFilename = join(Directory,OldFilename)
-	NewFilename = join(Directory,NewFilename)
-	print'Current Files are: ',repr(os.listdir(Directory))
-	print'The current file name is: ',repr(OldFilename)
-	print'The new file name will be: ',repr(NewFilename)
-	for filename in os.listdir(Directory):
-		os.rename(OldFilename, NewFilename)
-
-# def runTest():
-# 	print('Write Test Here')
-	
-changeName(OldFilename,NewFilename,Directory)
+def changeName(OldFilename,NewFilename):
+	os.rename(OldFilename, NewFilename)
+	print "Success! Go have a spot o' tea."
 
 
+def runTest(OldFilename,NewFilename,Directory):
+	OldFilename = joinDirectoryAndName(Directory,OldFilename)
+	NewFilename = joinDirectoryAndName(Directory,NewFilename)
+	existsOld = os.path.isfile(OldFilename)
+	existsNew = os.path.isfile(NewFilename)
+	if existsOld and not existsNew:
+		print "Preparing to rename"
+		changeName(OldFilename,NewFilename)
+	elif not existsOld:
+		print "File ",OldFilename," does not exist"
+	elif existsNew:
+		print "File already exists"
+
+
+runTest(OldFilename,NewFilename,Directory)
+
+# Results:
 #// Successfully changes the name of the file.
 #// OSError: [Errno 2] No such file or directory
