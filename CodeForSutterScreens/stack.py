782f741a5b3866c2a3582f4b893d6df78b2892ed
ericgfx <ericgfx01@gmail.com>
2018-10-25 09:29:23 -0700

    create node

---
+++ CodeForSutterScreens/stack.py
@@ -0,0 +1,19 @@
+from node import Node
+
+class Stack:
+  def __init__(self):
+    self.top_item = None
+  
+  # Define your push() and pop() methods below:
+  def push(self, value):
+    item = Node(value)
+    item.set_next_node(self.top_item)
+    self.top_item = item
+
+  def pop(self):
+    item_to_remove = self.top_item
+    self.top_item = item_to_remove.get_next_node()
+    return item_to_remove.get_value()
+  
+  def peek(self):
+    return self.top_item.get_value()
